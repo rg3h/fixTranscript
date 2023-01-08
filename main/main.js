32fff6f3b4b31cbb3f46c66c6df60d260bc0d5e9
rg3h <rich.gossweiler@gmail.com>
2023-01-07 16:24:23 -0800

    initial checkin of the pagee

---
+++ main/main.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview fixTranscript/main/main.js landing page code.
+ */
+
+/*****
+import {updateDateOnTheMinute} from '../clientComponents/date/date.js';
+import {getFirstElementByClassName} from '../clientComponents/html/html.js';
+
+window.addEventListener('load', main);
+window.addEventListener('unload', function () {});  // break back button cache
+
+// main entry point for the app
+async function main() {
+  handleDate();
+}
+
+function handleDate() {
+  let dateDiv = getFirstElementByClassName('date');
+  updateDateOnTheMinute(dateDiv);
+}
+*****/
