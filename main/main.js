f2727aaa42326264fc3d4d46dc69a067ed6f3ab6
rg3h <rich.gossweiler@gmail.com>
2023-01-09 00:49:16 -0800

    starting to add in the copy paste UX

--- main/main.js
+++ main/main.js
@@ -2,20 +2,73 @@
  * @fileoverview fixTranscript/main/main.js landing page code.
  */
 
-/*****
-import {updateDateOnTheMinute} from '../clientComponents/date/date.js';
-import {getFirstElementByClassName} from '../clientComponents/html/html.js';
-
 window.addEventListener('load', main);
 window.addEventListener('unload', function () {});  // break back button cache
 
+let dropZoneMain;
+
 // main entry point for the app
 async function main() {
-  handleDate();
+  dropZoneMain = document.getElementsByClassName('dropZoneMainBeforeDrop')[0];
+  setEmptySelection();
+
+  // dropZoneMain.addEventListener('keypress', handleDropZoneKeyPress);
+  dropZoneMain.addEventListener('pointerdown', handleDropZonePointerDown);
+  dropZoneMain.addEventListener('paste', handlePaste);
 }
 
-function handleDate() {
-  let dateDiv = getFirstElementByClassName('date');
-  updateDateOnTheMinute(dateDiv);
+function handleDropZonePointerDown(e) {
+  e.preventDefault;
+  setEmptySelection();
 }
-*****/
+
+function setEmptySelection() {
+  dropZoneMain.innerHTML = '\u00A0';
+  dropZoneMain.focus();
+  let selection = window.getSelection();
+  // selection.modify('extend', 'forward', 'line');  // selects the entire line
+  let firstChild = dropZoneMain.firstChild;
+  selection.collapse(firstChild, firstChild.length/2);
+}
+
+function handleDropZoneKeyPress(e) {
+  e.preventDefault;
+
+  if (firstTime) {
+    e.target.innerHTML = '';
+    firstTime = false;
+  }
+  let name = e.key;
+  let code = e.code;
+  if (e.key === 'Enter') {
+    console.log(buffer.join(''));
+  } else {
+    console.log(e.key);
+    buffer.push(e.key);
+  }
+}
+
+function handlePaste(e) {
+  e.preventDefault;
+
+  dropZoneMain.className = 'dropZoneMain';
+
+  let text = (e.clipboardData || window.clipboardDats).getData('text');
+  console.log(text);
+  text = fixText(text);
+  let result = document.getElementsByClassName('resultsZoneMain')[0];
+  result.innerHTML = text;
+
+/***
+  setTimeout(function() {
+    let result = document.getElementsByClassName('resultsZoneMain')[0];
+    result.innerHTML = null;
+    let clone = dropZoneMain.cloneNode(true);
+    result.appendChild(clone);
+  }, 0);
+***/
+}
+
+function fixText(textIn) {
+  return textIn.toUpperCase();
+}
