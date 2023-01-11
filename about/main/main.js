e2fa59bbe4335be24ddd3bcdd2a562579e1a87ad
rg3h <rich.gossweiler@gmail.com>
2023-01-11 11:54:16 -0800

    fixed about back button

--- about/main/main.js
+++ about/main/main.js
@@ -13,10 +13,21 @@ window.addEventListener('unload', function () {});  // break back button cache
 // main entry point for the app
 async function main() {
   createHeader();
+  createBackButton();
 }
 
 function createHeader() {
   getFirstElementByClassName('logoVersion').innerHTML = VERSION;
   let dateEle = getFirstElementByClassName('headerDate');
   updateDateOnTheMinute(dateEle, 'MMMM DTH, YYYY HH:NN AMPM');
+}
+
+function createBackButton() {
+  let backButton = getFirstElementByName('backButton');
+  backButton.addEventListener('pointerup', handleBackButton);
+  return backButton;
+}
+
+function handleBackButton(e) {
+  window.location.href = '../index.html';
 }
